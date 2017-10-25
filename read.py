fdafd7018d1b76acaccf8b574eb6c87c3299bef2
pko89403 <65alswo!>
2017-10-25 13:12:52 +0900

    Complete Read All of the Simulation Raw Data and put zone into dataframe then that move in to the list per date.

--- read.py
+++ read.py
@@ -1,14 +1,11 @@
 print("Hello world!!!")
 import os
-from pandas import DataFrame as df
+from pandas import DataFrame
 import numpy as np
 
 LAST_ZONE=13
 SIM_DIR='./Simulation/'
-
-
-
-
+TIME_CNT=37
 
 def readSimDir(dirName):
     list = []
@@ -23,27 +20,44 @@ def readSimDir(dirName):
 def readSimFile(fileName):
     f = open(fileName, 'r')
     line = f.readlines()
-    print(line)
+    line = list(map(int,line))
     f.close()
+    return line
 
 def makeDframe(zoneList):
+    df = DataFrame(index=range(1,TIME_CNT+1))
+    i=0
+    for zone in zoneList:
+        r_tmp = readSimFile(zone)
+        zone = 'zone'+ str(i+1)
+        df.insert(column=zone, value=r_tmp, loc=(i) )
+        i = i + 1
+    return df
 
-    tmp = df(columns=('zone1','zone2','zone3','zone4','zone5','zone6','zone7','zone8','zone9','zone10','zone11','zone12'))
 
+def read_RawData():
+    simData = {}
+    SimDates = readSimDir(SIM_DIR)  # SimDates is Simulation date list
 
-
-def noName():
-    SimDates = readSimDir(SIM_DIR)
     for simDate in SimDates:
-        print('--')
         zoneList = []
-        for zoneNum in range(1,LAST_ZONE):
+
+        for zoneNum in range(1,LAST_ZONE): # make zone list 1 to 12. zoneList is a list that contains path of the zone1.txt to zone12.txt
             zoneName = os.path.join(simDate, 'zone'+str(zoneNum)+'.txt')
             zoneList.append(zoneName)
-        makeDframe(zoneList)
+
+        dateDframe = makeDframe(zoneList)
+
+        simDate = simDate.split('/')[-1]
+        simData[simDate]=dateDframe
+    return simData
 
 
 
 
 
-noName()
+
+raw_Data = read_RawData()
+raw_Data_Keys = list(raw_Data.keys())
+print(raw_Data_Keys[1])
+print(raw_Data[raw_Data_Keys[1]])
